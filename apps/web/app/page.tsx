980865f6028d9a04c600b742309a6efb78f3466a
Mubashir Hassan <official.mhm13@gmail.com>
2023-06-11 03:02:30 +0500

    feat: setup caching for environment vars

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from "ui";
 export default function Page() {
   return (
     <>
-      <Header text="Web" />
+      <Header text={`Web - ${process.env.NEXT_PUBLIC_NAME}`} />
       <Button />
     </>
   );
