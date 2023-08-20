96db80209899ab5c077d1edef78558d68fa2303d
Mubashir Hassan <official.mhm13@gmail.com>
2023-08-20 17:57:45 +0500

    fix: make config module global

--- apps/api/src/config/config.module.ts
+++ apps/api/src/config/config.module.ts
@@ -1,9 +1,10 @@
-import { DynamicModule, Module } from "@nestjs/common";
+import { DynamicModule, Global, Module } from "@nestjs/common";
 import { ConfigModule as ConfigurationModule } from "@nestjs/config";
 import { ConfigService } from "./config.service";
 import { validateConfig } from "./helpers";
 
 @Module({})
+@Global()
 export class ConfigModule {
   static register(nodeEnv: string): DynamicModule {
     if (!nodeEnv) {
